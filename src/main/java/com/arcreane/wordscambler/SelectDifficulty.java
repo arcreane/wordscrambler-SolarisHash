9263ac25f5d716b6108b78cb3416b5a49775bd72
SolarisHash <77726918+SolarisHash@users.noreply.github.com>
2023-09-28 17:33:26 +0200

    Lien entre la fenetre selection et la fenetre de jeu

--- src/main/java/com/arcreane/wordscambler/SelectDifficulty.java
+++ src/main/java/com/arcreane/wordscambler/SelectDifficulty.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class SelectDifficulty {
     @FXML
@@ -22,8 +23,7 @@ public class SelectDifficulty {
 
     @FXML
     protected void easy(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("new-view1.fxml"));
-        Parent root = fxmlLoader.load();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
         Scene scene = new Scene(root, 800, 400);
         Stage stage = (Stage) buttonEasy.getScene().getWindow(); // Obtenez le Stage actuel
         stage.setScene(scene);
@@ -31,8 +31,7 @@ public class SelectDifficulty {
 
     @FXML
     protected void medium(ActionEvent event) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("new-view2.fxml"));
-        Parent root = fxmlLoader.load();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
         Scene scene = new Scene(root, 800, 400);
         Stage stage = (Stage) buttonMedium.getScene().getWindow(); // Obtenez le Stage actuel
         stage.setScene(scene);
@@ -40,8 +39,7 @@ public class SelectDifficulty {
 
     @FXML
     protected void hard(ActionEvent event) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("new-view3.fxml"));
-        Parent root = fxmlLoader.load();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
         Scene scene = new Scene(root, 800, 400);
         Stage stage = (Stage) buttonHard.getScene().getWindow(); // Obtenez le Stage actuel
         stage.setScene(scene);
